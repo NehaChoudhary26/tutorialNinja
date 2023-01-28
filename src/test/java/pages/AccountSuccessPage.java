74f3d5df4695abb92b93a41ca4c9d3ba77a97b03
NehaChoudhary26 <neha@stockholmitacademy.org>
2023-01-28 16:05:47 +0100

    commits

---
+++ src/test/java/pages/AccountSuccessPage.java
@@ -0,0 +1,19 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class AccountSuccessPage {
+	
+	WebDriver driver;
+	By registerAccountSuccessMsg = By.id("content");
+	
+	public AccountSuccessPage(WebDriver driver) {
+		this.driver = driver;	
+	}
+	public String getAccountRegisterSuccessMsg() {
+		String registerSuccessMsg = driver.findElement(registerAccountSuccessMsg).getText();
+		return registerSuccessMsg;
+	}
+
+}
