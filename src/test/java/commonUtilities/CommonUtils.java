105979bd802250e9e02a74b190a56ef928313d5c
NehaChoudhary26 <neha@stockholmitacademy.org>
2023-01-28 15:58:08 +0100

    commits

---
+++ src/test/java/commonUtilities/CommonUtils.java
@@ -0,0 +1,39 @@
+package commonUtilities;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class CommonUtils {
+	public String captureScreenShot(WebDriver driver,String testName) 
+	{
+		TakesScreenshot screenshot = ((TakesScreenshot)driver);
+	
+		File sourceScreenshot = screenshot.getScreenshotAs(OutputType.FILE);
+		String destinationPath = System.getProperty("user.dir") + "/Screenshots/" + testName + ".png";
+		File destinationScreenshot = new File(destinationPath);
+		try {
+			FileUtils.copyFile(sourceScreenshot,destinationScreenshot);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return destinationPath;
+	}
+
+ public static String generateRandomString() {
+	 String generatedstring=RandomStringUtils.randomAlphabetic(8);
+		return(generatedstring);
+	}
+ 
+ public static String generateRandomeNumber() {
+		String generatedString2 = RandomStringUtils.randomNumeric(4);
+		return (generatedString2);
+	}
+
+}
